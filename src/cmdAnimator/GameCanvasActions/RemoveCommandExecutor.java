78a2c21afcc4fef2993a85bb6f636de2631ab992
Mack <mckenziecunningham101@yahoo.com>
2016-10-14 20:53:22 -0400

    added remove class

---
+++ src/cmdAnimator/GameCanvasActions/RemoveCommandExecutor.java
@@ -0,0 +1,17 @@
+package cmdAnimator.GameCanvasActions;
+
+public class RemoveCommandExecutor implements ICommandExecutor {
+
+	@Override
+	public void execute(String[] parameters) throws InvalidCommandException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getErrorType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/cmdAnimator/GameCanvasActions/RemoveCommandExecutorTests.java
@@ -0,0 +1,14 @@
+package cmdAnimator.GameCanvasActions;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RemoveCommandExecutorTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
